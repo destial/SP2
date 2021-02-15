122a13093b2723cb466397f74cd2c3445883072d
zshaqeel <69779056+zshaqeel@users.noreply.github.com>
2021-02-15 15:57:30 +0800

    sceneshaq

--- Application/Source/SceneShaq.cpp
+++ Application/Source/SceneShaq.cpp
@@ -12,7 +12,7 @@ SceneShaq::SceneShaq() {}
 
 SceneShaq::~SceneShaq() {}
 
-void SceneShaq::Init()
+void SceneShaq::Init() // new change
 {
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f); //bg colour
 
@@ -32,7 +32,7 @@ void SceneShaq::Init()
 	Mtx44 projection;
 	projection.SetToPerspective(45.f, 4.f / 3.f, 0.1f, 1000.f);
 	projectionStack.LoadMatrix(projection);
-	camera.Init(Vector3(-1, 6, -1), Vector3(0, 5, 0), Vector3(0, 1, 0), (float)50);
+	camera.Init(Vector3(-1, 6, -1), Vector3(0, 5, 0), Vector3(0, 1, 0), (float)50); //
 
 	//shaders
 	glGenVertexArrays(1, &m_vertexArrayID);

84bd67e01247bfac3ebe279d249e2e4d7128026d
KneeGah <190874h@mymail.nyp.edu.sg>
2021-02-23 09:49:39 +0800

    fixed error

--- Application/Source/SceneXL.cpp
+++ Application/Source/SceneXL.cpp
@@ -522,8 +522,8 @@ void SceneXL::InitGL()
 	// Make sure you pass uniform parameters after glUseProgram()
 
 	//Replace previous code
-	light[0].type = Light::LIGHT_DIRECTIONAL;
-	light[0].position.Set(0, 50, 0);
+	//light[0].type = Light::LIGHT_POINT;
+	light[0].position.Set(0, 0, 0);
 	light[0].color.Set(1, 1, 1);
 	light[0].power = 1;
 	light[0].kC = 1.f;
